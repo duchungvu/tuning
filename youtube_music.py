346541a51be8dc8fc42dea1e2148de8dc25542e5
Hung Vu <duchungvu1999@gmail.com>
2020-10-12 00:32:53 -0700

    Change output

--- youtube_music.py
+++ youtube_music.py
@@ -22,12 +22,13 @@ def get_playlist_info(playlist):
     offset = 0
     playlist_tracks = sp.playlist_items(playlist_id=uri, offset=offset)
     total = len(playlist_tracks['items'])
-    while total > 0:
+    while offset < total:
         for track in playlist_tracks['items']:
             offset += 1
             track_name = track['track']['name']
             artist_name = track['track']['artists'][0]['name']
             if track_name == "":
+                print("[{}/{}] Skipped".format(offset, total))
                 continue
             query = track_name + " " + artist_name
 
@@ -37,37 +38,46 @@ def get_playlist_info(playlist):
             enablePrint()
 
             if len(music_songs_search) > 0:
-                print("[{}/{}] Music song search: {} | {}".format(offset, total, music_songs_search[0]['title'],
-                                                                  track_name))
-                video_id = music_songs_search[0]['videoId']
+                searched_type = "Music song"
+                searched = music_songs_search[0]
             elif len(music_videos_search) > 0:
-                print("[{}/{}] Music video search: {} | {}".format(offset, total, music_songs_search[0]['title'],
-                                                                   track_name))
-                video_id = music_songs_search[0]['videoId']
+                searched_type = "Music video"
+                searched = music_songs_search[0]
             else:
                 video_search = youtube.search().list(q=query, part="snippet", maxResults=1).execute()
-                print("[{}/{}] Video search: {}".format(offset, total, video_search['items'][0]['snippet']['title'],
-                                                        track_name))
-                video_id = video_search['items'][0]['id']['videoId']
+                searched_type = "Video"
+                searched = video_search['items'][0]['snippet']
 
-            if video_id:
-                video_ids.append(video_id)
+            if searched:
+                if searched['videoId'] not in video_ids:
+                    video_ids.append(searched['videoId'])
+                    print("[{}/{}] {} search: {} | {}".format(offset, total, searched_type, track_name,
+                                                              searched['title']))
+                else:
+                    print("[{}/{}] {} search: {} | {} | DUPLICATED".format(offset, total, searched_type, track_name, searched['title']))
+            else:
+                print("[{}/{}] Nothing found: {}".format(offset, total, track_name))
 
         playlist_tracks = sp.playlist_items(playlist_id=uri, offset=offset)
-        total = len(playlist_tracks['items'])
+        total += len(playlist_tracks['items'])
 
     return title, video_ids
 
 
 def import_playlist(playlist):
     title, video_ids = get_playlist_info(playlist)
+    print(title)
+    print(video_ids)
     print("Creating playlist: {}, with {} songs...".format(title, len(video_ids)))
-    ytmusic.create_playlist(title, description="", video_ids=video_ids)
+    res = ytmusic.create_playlist(title, description="", video_ids=video_ids)
     print("Created playlist: {}, with {} songs.".format(title, len(video_ids)))
+    print(res)
 
 
 def import_all_playlists():
     playlists = sp.current_user_playlists()
+    playlist = playlists['items'][0]
+    import_playlist(playlist)
     for playlist in playlists['items']:
         import_playlist(playlist)
 
